610e3a46f79873a44f00e9b8094dfae090b7fac7
tsk <tsk.nakamura@gmail.com>
2025-11-29 11:03:22 +0100

    db

--- api/src/main.rs
+++ api/src/main.rs
@@ -36,10 +36,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // TODO: clean up
     let db_options = PgConnectOptions::new()
+        .host("127.0.0.1")
+        .port(5432)
         .username("postgres")
         .password(r#"5d2%^7+&KK`;E<T"#)
         .database("postgres")
-        .host("/cloudsql/txgraph-475814:us-central1:tx-graph-db");
+        .host("txgraph-475814:us-central1:tx-graph-db");
+    // .host("/cloudsql/txgraph-475814:us-central1:tx-graph-db");
 
     let pool = PgPoolOptions::new().connect_with(db_options).await?;
     info!("Connected to database");

6cfa2497925e9eaf1365aee4dc14b70742bbe02d
tsk <tsk.nakamura@gmail.com>
2025-12-01 08:01:55 +0100

    .env sample

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # react-svg-digraph
 
+
+# Run locally
+
+Run `postgres`
 ```shell
 docker run --name rust-postgres-db \
 --rm \
@@ -9,22 +13,33 @@ docker run --name rust-postgres-db \
 -p 5432:5432 \
 -d postgres
 
+# Optional persistent data storage
 -v pgdata:/var/lib/postgresql/data \
+```
 
+Run `api` server
+```
 cargo install sqlx-cli --no-default-features --features native-tls,postgres
 
 DATABASE_URL=postgres://<username>:<password>@<host>:<port>/<database>
 
 cd api
 
-# Create new migration file
-sqlx migrate add create_table_contracts
-
 sqlx database create
 sqlx migrate run
 cargo sqlx prepare
 
+cargo run
+
+# Build and run using docker
 docker build . -t tx-graph-api
 docker run --name tx-api --rm -p 8080:8080 --network host --env-file .env tx-graph-api
+
+# Execute this command to create new migration file
+sqlx migrate add create_table_contracts
 ```
 
+Run `ui`
+```shell
+npm run dev
+```
--- ui/src/api/index.ts
+++ ui/src/api/index.ts
@@ -1,10 +1,30 @@
 import { post, get } from "./lib"
 import { TxCall, EtherscanContractInfo, ContractInfo } from "./types"
 
+export const RPC_URLS = {
+  "eth-mainnet": import.meta.env.VITE_ETH_MAINNET_RPC_URL,
+  "eth-sepolia": import.meta.env.VITE_ETH_SEPOLIA_RPC_URL,
+  "arb-mainnet": import.meta.env.VITE_ARB_MAINNET_RPC_URL,
+  "arb-sepolia": import.meta.env.VITE_ARB_SEPOLIA_RPC_URL,
+  "base-mainnet": import.meta.env.VITE_BASE_MAINNET_RPC_URL,
+  "base-sepolia": import.meta.env.VITE_BASE_SEPOLIA_RPC_URL,
+  //"bnb-mainnet": import.meta.env.VITE_BNB_MAINNET_RPC_URL,
+  // "bnb-testnet": import.meta.env.VITE_BNB_TESTNET_RPC_URL,
+  //"opt-mainnet": import.meta.env.VITE_OPT_MAINNET_RPC_URL,
+  //"opt-sepolia": import.meta.env.VITE_OPT_SEPOLIA_RPC_URL,
+  "hyperliquid-mainnet": import.meta.env.VITE_HYPERLIQUID_MAINNET_RPC_URL,
+  "monad-mainnet": import.meta.env.VITE_MONAD_MAINNET_RPC_URL,
+  "monad-testnet": import.meta.env.VITE_MONAD_TESTNET_RPC_URL,
+  "unichain-mainnet": import.meta.env.VITE_UNICHAIN_MAINNET_RPC_URL,
+  "unichain-sepolia": import.meta.env.VITE_UNICHAIN_SEPOLIA_RPC_URL,
+  "polygon-mainnet": import.meta.env.VITE_POLYGON_MAINNET_RPC_URL,
+  "polygon-amoy": import.meta.env.VITE_POLYGON_AMOY_RPC_URL,
+  "zksync-mainnet": import.meta.env.VITE_ZKSYNC_MAINNET_RPC_URL,
+  "zksync-sepolia": import.meta.env.VITE_ZKSYNC_SEPOLIA_RPC_URL,
+}
+
 // TODO: fix reloading page results in 404
 export async function getTxTrace(txHash: string): Promise<{ result: TxCall }> {
-  // TODO: remove
-  console.log("DEBUG", import.meta.env)
   return post<any, { result: TxCall }>(import.meta.env.VITE_RPC_URL, {
     jsonrpc: "2.0",
     method: "debug_traceTransaction",
@@ -42,8 +62,6 @@ export async function getContracts(params: {
   chain_id: number
   addrs: string[]
 }): Promise<ContractInfo[]> {
-  // TODO: remove
-  console.log("DEBUG", import.meta.env)
   return post<any, ContractInfo[]>(
     `${import.meta.env.VITE_API_URL}/contracts`,
     params,

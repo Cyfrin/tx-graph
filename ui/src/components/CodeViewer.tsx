cb9340d431c00a98f2fb09a2daa7b5efe8f2186b
t4sk <tsk.nakamura@gmail.com>
2026-02-23 12:21:57 +0100

    fix syntax highlighter

--- ui/src/components/CodeViewer.tsx
+++ ui/src/components/CodeViewer.tsx
@@ -1,6 +1,10 @@
 import React from "react"
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter"
+import solidity from "react-syntax-highlighter/dist/esm/languages/prism/solidity"
 import { atomDark as theme } from "react-syntax-highlighter/dist/esm/styles/prism"
+
+// Register to ship with production code
+SyntaxHighlighter.registerLanguage("solidity", solidity)
 
 const CodeViewer: React.FC<{
   text: string
